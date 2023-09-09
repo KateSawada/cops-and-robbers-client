9948c9bca998d627807743855ee1158e1b3dc491
KateSawada <nuksawada@gmail.com>
2023-09-09 18:20:22 +0900

    マーカーの動的な表示 #1

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,6 @@
 // 時刻を選択して，その時の位置情報が表示される．
 // デフォルトでは最新のものを表示
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -11,6 +12,47 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapAppState extends State<MapPage> {
+  LatLng nagoyaLatLng = const LatLng(35.1814, 136.9063);
+  late Marker marker;
+
+  @override
+  void initState() {
+    super.initState();
+    marker = Marker(
+        width: 32,
+        height: 32,
+        point: nagoyaLatLng,
+        builder: (ctx) => PersonMarkerButton(
+              radius: 16,
+              onPressed: randomWarp,
+              backGroundColor: Colors.red,
+            ));
+  }
+
+  double _randomDoubleWithRange(double min, double max) {
+    double value = math.Random().nextDouble() * (max - min);
+    return value + min;
+  }
+
+  void randomWarp() {
+    // マーカーをnagoyaLatLngを中心にランダムに移動させる
+    LatLng newLatLng = LatLng(
+        nagoyaLatLng.latitude + _randomDoubleWithRange(-0.01, 0.01),
+        nagoyaLatLng.longitude + _randomDoubleWithRange(-0.01, 0.01));
+
+    setState(() {
+      marker = Marker(
+          width: 32,
+          height: 32,
+          point: newLatLng,
+          builder: (ctx) => PersonMarkerButton(
+                radius: 16,
+                onPressed: randomWarp,
+                backGroundColor: Colors.red,
+              ));
+    });
+  }
+
   final String _title = 'map_app';
   @override
   Widget build(BuildContext context) {
@@ -27,7 +69,7 @@ class _MapAppState extends State<MapPage> {
         body: FlutterMap(
           // マップ表示設定
           options: MapOptions(
-            center: LatLng(35.681, 139.767),
+            center: nagoyaLatLng,
             zoom: 14.0,
           ),
           children: [
@@ -36,17 +78,7 @@ class _MapAppState extends State<MapPage> {
               userAgentPackageName: 'dev.fleaflet.flutter_map.example',
             ),
             MarkerLayer(
-              markers: [
-                Marker(
-                    width: 32,
-                    height: 32,
-                    point: const LatLng(48.8566, 2.3522),
-                    builder: (ctx) => PersonMarkerButton(
-                          radius: 16,
-                          onPressed: () {},
-                          backGroundColor: Colors.red,
-                        ))
-              ],
+              markers: [marker],
             ),
           ],
         ),
