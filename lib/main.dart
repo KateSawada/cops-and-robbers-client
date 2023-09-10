a5928e453d9e5769c54fc98edeedd4daa9f9bb7a
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-09-10 13:50:03 +0900

    feat: ローカル通知のサンプルコードを写経 #3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import "package:flutter_app_badger/flutter_app_badger.dart";
+import "package:flutter_local_notifications/flutter_local_notifications.dart";
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 import 'utils/footer.dart';
 import 'utils/initialize.dart';
 import 'utils/request.dart';
@@ -35,7 +39,106 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.removeObserver(this);
+    _init();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didCangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      FlutterAppBadger.removeBadge();
+    }
+  }
+
+  Future<void> _init() async {
+    await _configureLocalTimeZone();
+    await _initializeNotification();
+  }
+
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+  }
+
+  Future<void> _initializeNotification() async {
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+            requestAlertPermission: false,
+            requestBadgePermission: false,
+            requestSoundPermission: false);
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings("ic_notification");
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
+    );
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _cancelNotification() async {
+    await _flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+  Future<void> _requestPermissions() async {
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(alert: true, badge: true, sound: true);
+  }
+
+  Future<void> _registerMessage({
+    required int hour,
+    required int minutes,
+    required message,
+  }) async {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      hour,
+      minutes,
+    );
+
+    await _flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      "flutter_local_notifications",
+      message,
+      scheduledDate,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          'channel id',
+          'channel name',
+          importance: Importance.max,
+          priority: Priority.high,
+          ongoing: true,
+          styleInformation: BigTextStyleInformation(message),
+          icon: 'ic_notification',
+        ),
+        iOS: const DarwinNotificationDetails(
+          badgeNumber: 1,
+        ),
+      ),
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,7 +165,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             // TODO: ルームIDとユーザー名の入力
             ElevatedButton(
-              child: Text("ルームに参加"),
+              child: const Text("ルームに参加"),
               onPressed: () {
                 Navigator.push(
                     context,
@@ -70,6 +173,18 @@ class _MyHomePageState extends State<MyHomePage> {
                         builder: (context) => const RootWidget()));
               },
             ),
+            ElevatedButton(
+              child: const Text("Show Notification"),
+              onPressed: () async {
+                await _cancelNotification();
+                await _requestPermissions();
+                final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+                await _registerMessage(
+                    hour: now.hour,
+                    minutes: now.minute + 1,
+                    message: "hello, world!");
+              },
+            )
           ],
         ),
       ),
